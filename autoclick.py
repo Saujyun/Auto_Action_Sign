6cddce5ab606af0af8ae1270374ce3a8a9cf1434
lihongyi <lihongyi16@jd.com>
2021-04-05 14:28:31 +0800

    删除华工打卡，并修改页面元素获取路径

--- autoclick.py
+++ autoclick.py
@@ -3,42 +3,17 @@ import os
 import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import NoSuchElementException
 
 # 模拟浏览器打开网站
 chrome_options = Options()
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
-browser = webdriver.Chrome('/usr/bin/chromedriver', chrome_options=chrome_options)
-#window电脑本地
-# browser = webdriver.Chrome("C:\Program Files (x86)\Google\Chrome\Application\chromedriver")
-
-
-
-def scut():
-    browser.get('https://sso.scut.edu.cn/cas/login?service=https%3A%2F%2Fiamok.scut.edu.cn%2Fcas%2Flogin')
-    # 将窗口最大化
-    browser.maximize_window()
-    # 格式是PEP8自动转的
-    # 这里是找到输入框,发送要输入的用户名和密码,模拟登陆
-    browser.find_element_by_xpath(
-        "//*[@id='un']").send_keys(os.environ['SCUT_USER'])
-    browser.find_element_by_xpath(
-        "//*[@id='pd']").send_keys(os.environ['SCUT_PASSWORD'])
-    # 在输入用户名和密码之后,点击登陆按钮
-    browser.find_element_by_xpath("//*[@id='index_login_btn']").click()
-    time.sleep(50)
-    try:
-        browser.find_element_by_xpath("//*[@id='app']/div/div/div[2]/div[3]/button").click()
-        print("华工申报成功")
-        time.sleep(3)
-        saveFile("华工健康申报签到成功！")
-    except NoSuchElementException as e:
-        print ("NoSuchElementException!")
-        # js = 'document.getElementById("btn").click();'
-        # browser.execute_script(js)
-        saveFile("华工签到代码存在异常"+str(e))
-
+#windows电脑
+# browser = webdriver.Chrome('/usr/bin/chromedriver', chrome_options=chrome_options)
+#mac电脑调试将下面路径换成你的chromedriver本地路径即可，
+browser = webdriver.Chrome("/usr/local/bin/chromedriver")
 
 def saveFile(message):
     # 保存email内容
@@ -53,28 +28,27 @@ def situyun():
     # 格式是PEP8自动转的
     # 这里是找到输入框,发送要输入的用户名和密码,模拟登陆
     browser.find_element_by_xpath(
-        "//*[@id='email']").send_keys(os.environ['SITUYUN_USER'])
+        "/html/body/div[1]/section/div/div/div/div[2]/form/div/div[1]/input").send_keys("2679225466@qq.com")
     browser.find_element_by_xpath(
-        "//*[@id='password']").send_keys(os.environ['SITUYUN_PASSWORD'])
+        "/html/body/div[1]/section/div/div/div/div[2]/form/div/div[2]/input").send_keys("12345678")
     # 在输入用户名和密码之后,点击登陆按钮
-    browser.find_element_by_xpath("//*[@id='app']/section/div/div/div/div[2]/form/div/div[5]/button").click()
+    browser.find_element_by_xpath("/html/body/div[1]/section/div/div/div/div[2]/form/div/div[5]/button").click()
     time.sleep(10)
     try:
-        if("明日再来" in browser.find_element_by_xpath("//*[@id='checkin-div']").text):
+        if(browser.find_element_by_xpath("/html/body/div[2]/div/div/div[3]/button")!= None):
+            browser.find_element_by_xpath("/html/body/div[2]/div/div/div[3]/button").click()
+            time.sleep(5)
+        if("明日再来" in browser.find_element_by_xpath("/html/body/div[1]/div/div[3]/section/div[1]/div/div/a").text):
             saveFile("明日再来!")
         else:
-            # browser.find_element_by_xpath("//*[@id='checkin-div']/a").send_keys(Keys.ENTER)
-            js = 'document.getElementById("checkin-div").children[0].click();'
-            browser.execute_script(js)
-            print("司徒云打卡成功")
+            browser.find_element_by_xpath("/html/body/div[1]/div/div[3]/section/div[1]/div/div/a").click()
+            print("司徒云打卡成功2")
         time.sleep(3)
-        saveFile("司徒云签到成功！")
     except NoSuchElementException as e:
         print ("NoSuchElementException!")
         saveFile("司徒云签到代码存在异常"+str(e))
 
 if __name__ == '__main__':
-    scut()
     situyun()
     # 脚本运行成功,退出浏览器
     browser.quit()
